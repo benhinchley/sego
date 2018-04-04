ab1803afb829ca63c170d6c7de022604a7553598
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-04-04 20:08:22 +1000

    segmenter: add LoadDictionaryFromReader method

--- segmenter.go
+++ segmenter.go
@@ -4,6 +4,7 @@ package sego
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"math"
 	"os"
@@ -125,6 +126,94 @@ func (seg *Segmenter) LoadDictionary(files string) {
 	log.Println("sego词典载入完毕")
 }
 
+// LoadDictionaryFromReader loads a dictionary from the provided io.Reader
+func (seg *Segmenter) LoadDictionaryFromReader(r io.Reader) {
+	seg.dict = NewDictionary()
+
+	reader := bufio.NewReader(r)
+	var text string
+	var freqText string
+	var frequency int
+	var pos string
+
+	// 逐行读入分词
+	// Read segmentation line by line
+	for {
+		size, _ := fmt.Fscanln(reader, &text, &freqText, &pos)
+
+		if size == 0 {
+			// 文件结束
+			// End of file
+			break
+		} else if size < 2 {
+			// 无效行
+			// invalid line
+			continue
+		} else if size == 2 {
+			// 没有词性标注时设为空字符串
+			// set to an empty string without a part of speech tag
+			pos = ""
+		}
+
+		// 解析词频
+		// Analyze word frequency
+		var err error
+		frequency, err = strconv.Atoi(freqText)
+		if err != nil {
+			continue
+		}
+
+		// 过滤频率太小的词
+		// Filter words that are too small
+		if frequency < minTokenFrequency {
+			continue
+		}
+
+		// 将分词添加到字典中
+		// Add participles to the dictionary
+		words := splitTextToWords([]byte(text))
+		token := Token{text: words, frequency: frequency, pos: pos}
+		seg.dict.addToken(token)
+	}
+
+	// 计算每个分词的路径值，路径值含义见Token结构体的注释
+	// Calculate the path value of each participle.
+	// For the meaning of the path value, see the annotation of the Token structure
+	logTotalFrequency := float32(math.Log2(float64(seg.dict.totalFrequency)))
+	for i := range seg.dict.tokens {
+		token := &seg.dict.tokens[i]
+		token.distance = logTotalFrequency - float32(math.Log2(float64(token.frequency)))
+	}
+
+	// 对每个分词进行细致划分，用于搜索引擎模式，该模式用法见Token结构体的注释。
+	// Make a careful division of each participle for the search engine pattern.
+	// For usage of this pattern, see Token structure comments.
+	for i := range seg.dict.tokens {
+		token := &seg.dict.tokens[i]
+		segments := seg.segmentWords(token.text, true)
+
+		// 计算需要添加的子分词数目
+		// Calculate the number of subparticiples that need to be added
+		numTokensToAdd := 0
+		for iToken := 0; iToken < len(segments); iToken++ {
+			if len(segments[iToken].token.text) > 0 {
+				numTokensToAdd++
+			}
+		}
+		token.segments = make([]*Segment, numTokensToAdd)
+
+		// 添加子分词
+		// Add child segmentation
+		iSegmentsToAdd := 0
+		for iToken := 0; iToken < len(segments); iToken++ {
+			if len(segments[iToken].token.text) > 0 {
+				token.segments[iSegmentsToAdd] = &segments[iToken]
+				iSegmentsToAdd++
+			}
+		}
+	}
+}
+
 // 对文本分词
 //
 // 输入参数：
@@ -135,7 +224,6 @@ func (seg *Segmenter) LoadDictionary(files string) {
 func (seg *Segmenter) Segment(bytes []byte) []Segment {
 	return seg.internalSegment(bytes, false)
 }
-
 
 func (seg *Segmenter) InternalSegment(bytes []byte, searchMode bool) []Segment {
 	return seg.internalSegment(bytes, searchMode)
